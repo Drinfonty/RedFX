dacd2dd1a923086940f968a022c45c930d96a5d2
Drinfonty <alex.pham.013@gmail.com>
2026-08-05 17:05:00 -0400

    feat(neoforge): integrate with NeoForge built-in configuration screen API

--- neoforge/src/main/java/com/drinfonty/redfx/neoforge/RedfxNeoForge.java
+++ neoforge/src/main/java/com/drinfonty/redfx/neoforge/RedfxNeoForge.java
@@ -1,9 +1,19 @@
 package com.drinfonty.redfx.neoforge;
 
 import com.drinfonty.redfx.RedfxMod;
+import com.drinfonty.redfx.client.gui.RedfxConfigScreen;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
+@Mod("redfx")
 public class RedfxNeoForge {
-    public RedfxNeoForge() {
+    public RedfxNeoForge(ModContainer container) {
         RedfxMod.init();
+        if (FMLEnvironment.getDist() == Dist.CLIENT) {
+            container.registerExtensionPoint(IConfigScreenFactory.class, (mc, parent) -> new RedfxConfigScreen(parent));
+        }
     }
 }
