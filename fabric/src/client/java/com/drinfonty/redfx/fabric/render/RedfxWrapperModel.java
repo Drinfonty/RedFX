c17599f4fe8422c6181b2f167c5b19f8821d6ba1
Drinfonty <alex.pham.013@gmail.com>
2026-09-19 17:57:57 -0400

    Support translucent render pass for soft splatter edges on Fabric 26.3

--- fabric/src/client/java/com/drinfonty/redfx/fabric/render/RedfxWrapperModel.java
+++ fabric/src/client/java/com/drinfonty/redfx/fabric/render/RedfxWrapperModel.java
@@ -84,7 +84,8 @@ public class RedfxWrapperModel extends WrapperBlockStateModel {
 
 		emitter.nominalFace(back ? direction.getOpposite() : direction);
 		emitter.cullFace(null);
-		emitter.chunkLayer(ChunkSectionLayer.CUTOUT);
+		boolean isTranslucent = (argb >>> 24) < 255;
+		emitter.chunkLayer(isTranslucent ? ChunkSectionLayer.TRANSLUCENT : ChunkSectionLayer.CUTOUT);
 		emitter.emit();
 	}
 
